425c0606a857d6338072f2d61ccf414cf7f268e4
Tomo Hung <tomohung@gmail.com>
2017-11-02 11:32:11 +0800

    import test data crom cgg-data.edn

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -1,11 +1,29 @@
 (ns user
   (:require
    [clojure-game-geek.schema :as s]
-   [com.walmartlabs.lacinia :as lacinia]))
+   [com.walmartlabs.lacinia :as lacinia]
+   [clojure.walk :as walk])
+  (:import (clojure.lang IPersistentMap)))
 
 (def schema (s/load-schema))
 
+(defn simplify
+  "Convert all ordered maps nested within the map into standard hash maps, and sequences into vectors, which makes for easier contants in the tests, and eliminates order problems."
+  [m]
+  (walk/postwalk
+   (fn [node]
+     (cond
+       (instance? IPersistentMap node)
+       (into {} node)
+
+       (seq? node)
+       (vec node)
+
+       :else
+       node))
+   m))
+
 (defn q
   [query-string]
-  (lacinia/execute schema query-string nil nil))
-
+  (-> (lacinia/execute schema query-string nil nil)
+      simplify))
